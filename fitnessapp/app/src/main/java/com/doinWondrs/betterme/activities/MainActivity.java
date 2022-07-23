01ccc9dc1943af336d449dc041e9fb2ef98a2543
raul <zarate.rauljr@gmail.com>
2022-07-23 12:01:46 -0700

    added a scalar and logic to video render

--- fitnessapp/app/src/main/java/com/doinWondrs/betterme/activities/MainActivity.java
+++ fitnessapp/app/src/main/java/com/doinWondrs/betterme/activities/MainActivity.java
@@ -8,8 +8,15 @@ import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.Toast;
 import android.widget.VideoView;
 
 import com.doinWondrs.betterme.R;
@@ -18,9 +25,15 @@ import com.doinWondrs.betterme.helpers.GoToNav;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
     //field declaration
-    private GoToNav gotoHelper = new GoToNav();//TODO: do I need to instantiate?
+    private SurfaceView surfaceView;
+    private MediaPlayer player;
+    private SurfaceHolder holder;
+
+    private GoToNav gotoHelper = new GoToNav();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,12 +43,11 @@ public class MainActivity extends AppCompatActivity {
 
         delayIcon();
         navGoTo();
-        renderBackground();
+//        renderBackground();
+        renderBackground2();
     }
 
 
-    //TODO: Dynamicall use API for Moto quotes
-    //TODO: Use current auth user to Pull "nickname/username" and save User to use
 
 
 
@@ -54,28 +66,78 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    private void renderBackground()
+//    private void renderBackground()
+//    {
+//        final VideoView videoView = (VideoView) findViewById(R.id.videoBackground1);
+//        final String videopath = Uri.parse("android.resource://"+getPackageName() + "/" + R.raw.workout_intro).toString();
+//        videoView.setVideoPath(videopath);
+//        videoView.start();
+//        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+//            @Override
+//            public void onPrepared(MediaPlayer mediaPlayer) {
+//                mediaPlayer.start();
+//                mediaPlayer.setLooping(true);
+//            }
+//        });
+//        videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener(){
+//            @Override
+//            public void onCompletion(MediaPlayer mediaPlayer)
+//            {
+//                videoView.setVideoPath(videopath);
+//                videoView.start();
+//            }
+//        });
+//    }
+
+    private void renderBackground2()
     {
-        final VideoView videoView = (VideoView) findViewById(R.id.videoBackground1);
-        final String videopath = Uri.parse("android.resource://"+getPackageName() + "/" + R.raw.workout_intro).toString();
-        videoView.setVideoPath(videopath);
-        videoView.start();
-        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-            @Override
-            public void onPrepared(MediaPlayer mediaPlayer) {
-                mediaPlayer.start();
-                mediaPlayer.setLooping(true);
-            }
-        });
-        videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener(){
-            @Override
-            public void onCompletion(MediaPlayer mediaPlayer)
-            {
-                videoView.setVideoPath(videopath);
-                videoView.start();
-            }
-        });
+
+        //NOTES :  https://developpaper.com/android-uses-surfaceview-mediaplayer-to-play-video/
+        //NOTES : https://itecnote.com/tecnote/java-full-screen-videoview-without-stretching-the-video/
+        surfaceView = (SurfaceView) findViewById(R.id.videoBackground1);
+        player = new MediaPlayer();
+        try {
+            player.setDataSource(this, Uri.parse("android.resource://"+getPackageName() + "/" + R.raw.workout_intro) );
+            holder= surfaceView.getHolder();
+            holder.addCallback(new MyCallBack());
+            player.prepare();
+            player.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(MediaPlayer mediaPlayer) {
+
+
+                    // so it fits on the screen
+                    int videoWidth = player.getVideoWidth();
+                    int videoHeight = player.getVideoHeight();
+                    float videoProportion = (float) videoWidth / (float) videoHeight;
+                    int screenWidth = getWindowManager().getDefaultDisplay().getWidth();
+                    int screenHeight = getWindowManager().getDefaultDisplay().getHeight();
+                    float screenProportion = (float) screenWidth / (float) screenHeight;
+                    android.view.ViewGroup.LayoutParams lp = surfaceView.getLayoutParams();
+
+                    if (videoProportion > screenProportion) {
+                        lp.width = (1) * videoWidth;
+                        lp.height = (1/2) * videoWidth;
+//                        lp.width = screenWidth;
+//                        lp.height = (int) ((float) screenWidth / videoProportion);
+                    } else {
+                        lp.width = (int) (videoProportion * (float) screenHeight);
+                        lp.height = screenHeight;
+                    }
+                    surfaceView.setLayoutParams(lp);
+
+                    player.start();
+                    player.setLooping(true);
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
+
+
+
 
 
     //Bottom Navbar: NOTE: to link new activity just create a new switch cases and use new intents
@@ -122,5 +184,21 @@ public class MainActivity extends AppCompatActivity {
             }
         });//end lambda: bottomNavview
     }//end method: navGoTo
-}//end class
+    private class MyCallBack implements SurfaceHolder.Callback{
+
+        @Override
+        public void surfaceCreated(@NonNull SurfaceHolder surfaceHolder) {
+            player.setDisplay(holder);
+        }
+
+        @Override
+        public void surfaceChanged(@NonNull SurfaceHolder surfaceHolder, int format, int width, int height) {
+
+        }
+
+        @Override
+        public void surfaceDestroyed(@NonNull SurfaceHolder surfaceHolder) {
+        }
+    }//END: class MyCallback - implements sufaceholder.callback
+}//end class:
 
